0934dc1e6c78f56b1ba0dbe4e96fd82b58f90fd9
vipmaliya <93629493+vipmaliya@users.noreply.github.com>
2022-06-26 11:52:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94703139198']
+global.premium = ['94703139198']
+global.ownernomer = '94703139198'
+global.ownername = '❀⃝⃖🍏⃪⃪ₛͥₑͣᵪᷟʏ̸ мǟℓιуǟ ✓'
+global.botname = '❀⃝⃖🍏⃪⃪ₛͥₑͣᵪᷟʏ̸ мǟℓιуǟ ✓'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.author = '❀⃝⃖🍏⃪⃪ₛͥₑͣᵪᷟʏ̸ мǟℓιуǟ ✓'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
